2506256657e9aeadb43d8bda50dfa291d4d58c27
Juan Andrés Román Yáñez <andy_blest@hotmail.com>
2017-07-30 00:03:20 -0400

    Create 199A - Hexadecimal's theorem.cpp

---
+++ 199A - Hexadecimal's theorem.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#define optimizar_io ios_base::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+int main() {
+    optimizar_io
+    int a;
+    cin>>a;
+    cout<<a<<" 0 0\n";
+    return 0;
+}
