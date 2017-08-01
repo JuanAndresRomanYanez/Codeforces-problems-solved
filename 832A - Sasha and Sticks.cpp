b64c50cb7107235ed4ac26ba62c07677e1ac60e9
Juan Andrés Román Yáñez <andy_blest@hotmail.com>
2017-08-01 11:13:53 -0400

    Create 832A - Sasha and Sticks.cpp

---
+++ 832A - Sasha and Sticks.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define optimizar_io ios_base::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+int main(){
+    optimizar_io
+    long long a,b;
+    cin>>a>>b;
+    if((a/b)%2==0)cout<<"NO\n";
+    else cout<<"YES\n";
+    return 0;
+}
